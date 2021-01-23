4fa05199b7a6072d2d44a3732c43e1e6b2cbaac6
percevil <peeevil@gmail.com>
2021-01-22 20:26:13 -0800

    Add readme to repo.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+import React, {Component} from "react"
+import "./App.css"
+
+// https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/fetch
+// https://swapi.dev/
+// https://medium.com/javascript-scene/master-the-javascript-interview-what-is-a-promise-27fc71e77261
+
+
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            loading: false,
+            dogPic: {}
+        }
+    }
+    
+    componentDidMount() {
+        this.setState({loading: true})
+        fetch("https://dog.ceo/api/breeds/image/random")
+        //"https://dog.ceo/api/breeds/image/random"
+        // "https://dog.ceo/api/breeds/list/all"
+            .then(response => response.json())
+            .then(data => {
+                console.log(data)
+               
+                this.setState({
+                    loading: false,
+                    dogPic: data,   
+                })
+            }) 
+            this.buttonClick = this.buttonClick.bind(this)
+    }
+
+
+    buttonClick() {
+        this.setState({
+         
+        })
+          
+        
+    }
+
+   render() {
+
+     const displayPic = this.state.loading ? "Loading..." : this.state.dogPic.message
+       
+        return (
+            <div className="picContainer" >
+                 <img className="picture"src={displayPic} alt="cute puppies"></img>
+                 <button onClick="this.buttonClick" 
+                 className="button">Click for a New Dog</button>
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App
+
